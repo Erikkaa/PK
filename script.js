f191643de9d56883e922e12d72e4ffb5e098f980
Erika Mikalauskaitė <erikamikmik@gmail.com>
2020-01-27 21:59:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,350 @@
+function myMove()
+{
+  var elem1 = document.getElementById("myAnimation1");
+  var elem2 = document.getElementById("myAnimation2");
+  var elem3 = document.getElementById("myAnimation3");
+  var elem4 = document.getElementById("myAnimation4");
+  var elem5 = document.getElementById("myAnimation5");
+  var elem6 = document.getElementById("myAnimation6");
+  var elem7 = document.getElementById("myAnimation7");
+  var elem8 = document.getElementById("myAnimation8");
+  var elem9 = document.getElementById("myAnimation9");
+  var elem10 = document.getElementById("myAnimation10");
+  var elem11 = document.getElementById("myAnimation11");
+  var elem12 = document.getElementById("myAnimation12");
+  var elem13 = document.getElementById("myAnimation13");
+  var elem14 = document.getElementById("myAnimation14");
+  var elem15 = document.getElementById("myAnimation15");
+  var elem16 = document.getElementById("myAnimation16");
+  var elem17 = document.getElementById("myAnimation17");
+  var elem18 = document.getElementById("myAnimation18");
+  var elem19 = document.getElementById("myAnimation19");
+  var elem20 = document.getElementById("myAnimation20");
+  var elem21 = document.getElementById("myAnimation21");
+  var elem22 = document.getElementById("myAnimation22");
+  var elem23 = document.getElementById("myAnimation23");
+  var elem24 = document.getElementById("myAnimation24");
+  var elem25 = document.getElementById("myAnimation25");
+  var elem26 = document.getElementById("myAnimation26");
+  var elem27 = document.getElementById("myAnimation27");
+  var elem28 = document.getElementById("myAnimation28");
+  var elem29 = document.getElementById("myAnimation29");
+  var pos1 = 0;
+  var pos2 = 0;
+  var pos3 = 0;
+  var pos4 = 0;
+  var pos5 = 0;
+  var pos6 = 0;
+  var pos7 = 0;
+  var pos8 = 0;
+  var pos9 = 0;
+  var pos10 = 0;
+  var pos11 = 0;
+  var pos12 = 0;
+  var pos13 = 0;
+  var pos14 = 0;
+  var pos15 = 0;
+  var pos16 = 0;
+  var pos17 = 0;
+  var pos18 = 0;
+  var pos19 = 0;
+  var pos20 = 0;
+  var pos21 = 0;
+  var pos22 = 0;
+  var pos23 = 0;
+  var pos24 = 0;
+  var pos25 = 0;
+  var pos26 = 0;
+  var pos27 = 0;
+  var pos28 = 0;
+  var pos29 = 0;
+  var id = setInterval(frame, 20);
+  var id1 = setInterval(frame, 15);
+  var id2 = setInterval(frame, 10);
+function frame()
+{
+  if (pos1 == 150)
+  {
+    clearInterval(id);
+  }
+  else
+  {
+    pos1++;
+    elem1.style.top = pos1 + 'px';
+    elem1.style.left = pos1 + 'px';
+  }
+  if (pos2 == 150)
+  {
+    clearInterval(id);
+  }
+  else
+  {
+    pos2++;
+    elem2.style.top =pos2 + 'px';
+    elem2.style.left ='px';
+  }
+  if (pos3 == 150)
+  {
+    clearInterval(id);
+  }
+  else
+  {
+    pos3++;
+    elem3.style.top =pos3 + 'px';
+    elem3.style.left ='px';
+  }
+  if (pos4 == 150)
+  {
+    clearInterval(id);
+  }
+  else
+  {
+    pos4++;
+    elem4.style.top ='px';
+    elem4.style.left = pos4 + 'px';
+  }
+  if (pos5 == 150)
+  {
+    clearInterval(id);
+  }
+  else
+  {
+    pos5++;
+    elem5.style.top ='px';
+    elem5.style.left = pos5 + 'px';
+  }
+  if (pos6 == 150)
+  {
+    clearInterval(id);
+  }
+  else
+  {
+    pos6++;
+    elem6.style.top ='px';
+    elem6.style.left = pos6 + 'px';
+  }
+  if (pos7 == 150)
+  {
+    clearInterval(id);
+  }
+  else
+  {
+    pos7++;
+    elem7.style.top = pos7 + 'px';
+    elem7.style.left = 'px';
+  }
+  if (pos8 == 50)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos8++;
+    elem8.style.top = pos1 + 'px';
+    elem8.style.left = pos1 + 'px';
+  }
+  if (pos9 == 100 )
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos9++;
+    elem9.style.top =pos9 + 'px';
+    elem9.style.left ='px';
+  }
+  if (pos10 == 100)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos10++;
+    elem10.style.top ='px';
+    elem10.style.left = pos10 + 'px';
+  }
+  if (pos11 == 50)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos11++;
+    elem11.style.top ='px';
+    elem11.style.left = pos11 + 'px';
+  }
+  if (pos12 == 50)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos12++;
+    elem12.style.top ='px';
+    elem12.style.left = pos12 + 'px';
+  }
+  if (pos13 == 50)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos13++;
+    elem13.style.top = 'px';
+    elem13.style.left = pos13 + 'px';
+  }
+  if (pos14 == 50)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos14++;
+
+    elem14.style.bottom = pos14 + 'px' ;
+    elem14.style.left  = pos14 + 'px' ;
+  }
+  if (pos15 == 200)
+  {
+    clearInterval(id2);
+  }
+  else
+  {
+    pos15++;
+    elem15.style.bottom = pos15 + 'px';
+  }
+  if (pos16 == 200)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos16++;
+    elem16.style.right  = pos16 + 'px' ;
+  }
+  if (pos17 == 200)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos17++;
+    elem17.style.top  = pos17 + 'px' ;
+  }
+  if (pos18 == 150)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos18++;
+    elem18.style.bottom  = pos18 + 'px' ;
+  }
+  if (pos19 == 150)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos19++;
+    elem19.style.top  = pos19 + 'px' ;
+  }
+  if (pos20 == 100)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos20++;
+    elem20.style.bottom  = pos20 + 'px' ;
+  }
+  if (pos21 == 100)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos21++;
+    elem21.style.top  = pos21 + 'px' ;
+  }
+  if (pos22 == 50)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos22++;
+    elem22.style.bottom  = pos22 + 'px' ;
+  }
+  if (pos23 == 50)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos23++;
+    elem23.style.top  = pos23 + 'px' ;
+    elem23.style.right = pos23 + 'px' ;
+  }
+  if (pos24 == 75)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos24++;
+    elem24.style.bottom  = pos24 + 'px' ;
+    elem24.style.right = pos24 + 'px';
+  }
+  if (pos25 == 125)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos25++;
+    elem25.style.bottom  = pos25 + 'px' ;
+    elem25.style.right = pos25 + 'px';
+  }
+  if (pos26 == 125)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos26++;
+    elem26.style.top  = pos26 + 'px';
+  }
+  if (pos27 == 175)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos27++;
+    elem27.style.top  = pos27 + 'px';
+  }
+  if (pos28 == 50)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos28++;
+    elem28.style.left = pos28 + 'px';
+    elem28.style.top  = pos28 + 'px';
+
+  }
+  if (pos29 == 50)
+  {
+    clearInterval(id1);
+  }
+  else
+  {
+    pos29++;
+    elem29.style.top  = pos29 + 'px';
+    elem29.style.right = pos29 + 'px'
+  }
+
+}
+}
